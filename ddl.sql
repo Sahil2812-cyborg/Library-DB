a02ede904c0a0d82ec0e4a7f3e65e3e63af0006f
Sahil <sahilratnaparkhi1@gmail.com>
2025-07-01 13:28:06 +0530

    Added department column

--- ddl.sql
+++ ddl.sql
@@ -1,66 +1,69 @@
-CREATE TABLE books (
-  id INT NOT NULL,
-  author VARCHAR(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  name VARCHAR(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  pages INT DEFAULT NULL,
-  publication VARCHAR(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  created_on TIMESTAMP NULL DEFAULT NULL,
-  updated_on TIMESTAMP NULL DEFAULT NULL,
-  is_deleted TINYINT(1) DEFAULT NULL,
-  price FLOAT DEFAULT NULL,
-  PRIMARY KEY (id)
-);
+CREATE TABLE `book_instance` (
+  `id` int NOT NULL,
+  `book_id` int DEFAULT NULL,
+  `rack_no` int DEFAULT NULL,
+  `shelf_no` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_book_id` (`book_id`),
+  CONSTRAINT `fk_book_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `books` (
+  `id` int NOT NULL,
+  `author` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pages` int DEFAULT NULL,
+  `publication` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_on` timestamp NULL DEFAULT NULL,
+  `updated_on` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `borrows` (
+  `id` int NOT NULL,
+  `card_id` int DEFAULT NULL,
+  `book_id` int DEFAULT NULL,
+  `issue_date` timestamp NULL DEFAULT NULL,
+  `return_date` timestamp NULL DEFAULT NULL,
+  `is_damaged` tinyint(1) DEFAULT NULL,
+  `damage_charges` float DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL,
+  `book_instance_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_card_id` (`card_id`),
+  KEY `fk_book_2_id` (`book_id`),
+  KEY `fk_instance_id` (`book_instance_id`),
+  CONSTRAINT `fk_book_2_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `fk_card_id` FOREIGN KEY (`card_id`) REFERENCES `card` (`id`),
+  CONSTRAINT `fk_instance_id` FOREIGN KEY (`book_instance_id`) REFERENCES `book_instance` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-CREATE TABLE book_instance (
-  id INT NOT NULL,
-  book_id INT DEFAULT NULL,
-  rack_no INT DEFAULT NULL,
-  shelf_no INT DEFAULT NULL,
-  created_at TIMESTAMP NULL DEFAULT NULL,
-  updated_at TIMESTAMP NULL DEFAULT NULL,
-  is_deleted TINYINT(1) DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY fk_book_id (book_id),
-  CONSTRAINT fk_book_id FOREIGN KEY (book_id) REFERENCES books (id)
-);
+CREATE TABLE `card` (
+  `id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `card_no` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE users (
-  id INT NOT NULL,
-  name VARCHAR(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  phone VARCHAR(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  email VARCHAR(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  address VARCHAR(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  created_at TIMESTAMP NULL DEFAULT NULL,
-  updated_at TIMESTAMP NULL DEFAULT NULL,
-  is_deleted TINYINT(1) DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE card (
-  id INT NOT NULL,
-  user_id INT DEFAULT NULL,
-  card_no VARCHAR(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY fk_user_id (user_id),
-  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
-CREATE TABLE borrows (
-  id INT NOT NULL,
-  card_id INT DEFAULT NULL,
-  book_id INT DEFAULT NULL,
-  issue_date TIMESTAMP NULL DEFAULT NULL,
-  return_date TIMESTAMP NULL DEFAULT NULL,
-  is_damaged TINYINT(1) DEFAULT NULL,
-  damage_charges FLOAT DEFAULT NULL,
-  created_at TIMESTAMP NULL DEFAULT NULL,
-  updated_at TIMESTAMP NULL DEFAULT NULL,
-  is_deleted TINYINT(1) DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY fk_card_id (card_id),
-  KEY fk_book_2_id (book_id),
-  CONSTRAINT fk_book_2_id FOREIGN KEY (book_id) REFERENCES books (id),
-  CONSTRAINT fk_card_id FOREIGN KEY (card_id) REFERENCES card (id)
-);
-select * from borrows;
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL,
+  `department` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
